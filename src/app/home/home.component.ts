00275685756709f0d4b8d2d230bed8ffb0ef5d5d
theophiledesmedt <theophiledesmedt@gmail.com>
2023-06-03 19:21:10 +0200

    create a single instance of the new recipe interface in the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RecipeComponent } from "../recipe/recipe.component";
+import { Recipe } from "../models/recipe";
 
 @Component({
   selector: "app-home",
@@ -9,4 +10,27 @@ import { RecipeComponent } from "../recipe/recipe.component";
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+
+export class HomeComponent {
+  recipe: Recipe = {
+    name: "Gauffres",
+    ingredients: ["Oeufs", "farine", "lait", "sucre", "beurre", "sel", "levure"],
+    instructions: "Mélanger les ingrédients, laisser reposer, cuire au gaufrier",
+    cookingTime: 11,
+    difficulty: 'easy'
+  };
+}
+
+/*
+TODO: 
+
+* recipe list https://angular.io/tutorial/toh-pt2
+* recipe service https://angular.io/tutorial/toh-pt4
+* detail view https://angular.io/tutorial/toh-pt5
+* creation view https://angular.io/tutorial/toh-pt6
+* recipe model https://angular.io/tutorial/toh-pt6#add-the-herodata-model
+* recipe routing https://angular.io/tutorial/toh-pt5#add-the-approutingmodule
+* recipe guard https://angular.io/tutorial/toh-pt5#add-the-canactivate-route-guard
+* recipe resolver https://angular.io/tutorial/toh-pt6#add-a-resolver
+
+*/
--- src/app/models/recipe.ts
+++ src/app/models/recipe.ts
@@ -1,6 +1,6 @@
 export interface Recipe {
     name: string;
-    ingredients: string[];
+    ingredients: string[]; //example for the string array: ["Oeufs", "farine", "lait", "sucre", "beurre", "sel", "levure"]
     instructions: string;
     cookingTime: number;
     difficulty: 'easy' | 'medium' | 'hard';
