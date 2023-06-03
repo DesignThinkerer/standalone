6f7d573bb32a6216b22b1c1960d1b8da813c58b9
theophiledesmedt <theophiledesmedt@gmail.com>
2023-06-03 19:31:26 +0200

    Import the Input decorator

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Recipe } from '../models/recipe';
 
 @Component({
   selector: 'app-recipe',
