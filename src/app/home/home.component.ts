6c262dd01db4127178f31ad103a4fb5ec9b688c6
theophiledesmedt <theophiledesmedt@gmail.com>
2023-06-03 17:59:01 +0200

    Styles for home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
