a3005843fe21ed266c022d8f7710dc74c83f09ae
theophiledesmedt <theophiledesmedt@gmail.com>
2023-06-03 19:34:01 +0200

    quick explanation for the non null assertion operator

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -12,4 +12,5 @@ import { Recipe } from '../models/recipe';
 
 export class RecipeComponent {
   @Input() recipe!: RecipeComponent;
+  // Input except a value, but here there is no default value, so we use the ! is called the non-null assertion operator and it tells the TypeScript compiler that the value of this property won't be null or undefined.
 }
