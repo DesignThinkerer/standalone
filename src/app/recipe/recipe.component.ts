b297afc72b34cc20b5dc0e91ee88e32803849226
theophiledesmedt <theophiledesmedt@gmail.com>
2023-06-03 19:42:18 +0200

    add data binding in the home component template

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -11,6 +11,7 @@ import { Recipe } from '../models/recipe';
 })
 
 export class RecipeComponent {
-  @Input() recipe!: RecipeComponent;
+  @Input() recipe!: Recipe;
   // Input except a value, but here there is no default value, so we use the ! is called the non-null assertion operator and it tells the TypeScript compiler that the value of this property won't be null or undefined.
 }
+
