d71232c95cfdfb6ebaa4b11ddc6b25bb4cc6bc4e
theophiledesmedt <theophiledesmedt@gmail.com>
2023-06-03 17:46:25 +0200

    added router module to app component to enable nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'body', // changed from 'app-root' to benefit from the implicit aria roles of the HTML5 semantic elements
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
