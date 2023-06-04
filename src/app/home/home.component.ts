ac668becd079c57577c494287ba5c5908116a652
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-05 00:51:54 +0200

    added first implementation of recipe filtering based on name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,11 +15,32 @@ import { RecipeService } from "../services/recipe.service";
 
 export class HomeComponent {
   recipeList: Recipe[] = [];
-  filteredRecipeList: Recipe[] = []; //store data in a array property that will be used for filtering.
+  filteredRecipeList: Recipe[] = [];
+  //hold the values that match the search criteria entered by the user.
 
   recipeService: RecipeService = inject(RecipeService); //inject the RecipeService in the home component class
 
+
+  filterResults(text: string) {
+  /*
+  this function uses the String filter function to compare the value of the text parameter against the recipe.name property. This function can be updated to match against any property or multiple properties.
+  */
+    if (!text) {
+      this.filteredRecipeList = this.filteredRecipeList;
+    }
+  
+    this.filteredRecipeList = this.filteredRecipeList.filter(
+      recipe => recipe?.name.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
+
+
   constructor() {
-    this.recipeList = this.recipeService.getAllRecipe(); //get the recipe list from the RecipeService
+    this.recipeList = this.recipeService.getAllRecipe();
+
+    //filteredRecipeList should contain the total set of recipes by default when the page loads.
+
+    this.filteredRecipeList = this.recipeList;
   }
 }
