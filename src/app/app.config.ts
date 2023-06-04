3e996116438eef7b6f13ddfd59d48ece74b7a0ab
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-04 14:37:31 +0200

    Changes to implement routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
 
-import { routes } from './app.routes';
+import  routes  from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    // end-to-end testing
+    provideProtractorTestingSupport(), 
+    provideRouter(routes) ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
-export const routes: Routes = [];
+ const routes: Routes =  [
+	{
+	  path: '',
+	  component: HomeComponent,
+	  title: 'Home page'
+	},
+	{
+		path: 'details/:name',// see the interface for a recipe: src\app\models\recipe.ts
+		component: DetailsComponent,
+		title: 'Recipe details'
+	}
+  ];
+
+export default routes; //imported in app.config.ts
