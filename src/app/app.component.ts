399b6fcff2bff40e55edd18409094e4224ce702c
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-04 14:36:38 +0200

    working on adding routing to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+/*
+* routermodule is a module that provides the router service providers and directives needed for routing
+* routeroutlet is included in the routermodule
+*/
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'body', // changed from 'app-root' to benefit from the implicit aria roles of the HTML5 semantic elements
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
