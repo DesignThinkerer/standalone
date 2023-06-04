0434d3d5825b54bae4e87024c46385b1b58207e2
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-04 18:50:58 +0200

    Auto-generated commit [ci skip]

--- src/app/models/recipe.ts
+++
@@ -1,8 +0,0 @@
-export interface Recipe {
-    name: string;
-    photo: string;
-    ingredients: string[]; //example for the string array: ["Oeufs", "farine", "lait", "sucre", "beurre", "sel", "levure"]
-    instructions: string;
-    cookingTime: number;
-    difficulty: 'easy' | 'medium' | 'hard';
-  }
