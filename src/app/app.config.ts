67cebc2483a05ca6271b71a904c278eaa1ce8d0f
TheophileDesmedt <theophiledesmedt@gmail.com>
2023-06-05 15:42:05 +0200

    commenting out ionic strategy routing for now

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideProtractorTestingSupport } from '@angular/platform-browser';
-
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, RouteReuseStrategy } from '@angular/router';
+//import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import  routes  from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // end-to-end testing
-    provideProtractorTestingSupport(), 
-    provideRouter(routes) ]
+    // { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    // importProvidersFrom(IonicModule.forRoot({})),
+    //no idea what this is supposed to do - I'm just following this article: https://ionic.zendesk.com/hc/en-us/articles/10386373742231-Angular-Standalone-Components-with-Ionic
+    // So I'm commenting it out for now
+    //I assume it will be usefull for the routing strategy used by ionic
+    provideRouter(routes), 
+  ],
 };
